cc0fdccf207f4418efcf4f64acd70d3817e29097
siddhiBhosale18 <siddhibhosale1830@gmail.com>
2024-01-31 21:21:27 +0530

    Till HTML CSS

---
+++ app.js
@@ -0,0 +1,7 @@
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+for (code in countryList) {
+    console.log(code);
+}
